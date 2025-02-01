6b748b36648202d2ee6ad60f66253a4cc2d1764c
ich200 <amineichraq80@gmail.com>
2025-02-01 13:49:40 +0100

    Call useDebounce in ProductSearch

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,13 +1,14 @@
-import React, { useState, useContext } from 'react';
-import { ThemeContext } from '../App';
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "../App";
 
 const ProductSearch = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const { isDarkTheme } = useContext(ThemeContext);
   // TODO: Exercice 2.1 - Utiliser le LanguageContext
-  
+
   // TODO: Exercice 1.2 - Utiliser le hook useDebounce
-  
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+
   return (
     <div className="mb-4">
       <input
@@ -15,7 +16,7 @@ const ProductSearch = () => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
         placeholder="Rechercher un produit..."
-        className={`form-control ${isDarkTheme ? 'bg-dark text-light' : ''}`}
+        className={`form-control ${isDarkTheme ? "bg-dark text-light" : ""}`}
       />
     </div>
   );
