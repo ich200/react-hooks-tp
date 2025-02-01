eb9eee6788842d9416cf7be960faef2b11484805
ich200 <amineichraq80@gmail.com>
2025-02-01 14:02:23 +0100

    use useDebounce hook

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -7,14 +7,22 @@ const ProductSearch = () => {
   // TODO: Exercice 2.1 - Utiliser le LanguageContext
 
   // TODO: Exercice 1.2 - Utiliser le hook useDebounce
-  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  useEffect(() => {
+    onSearch(debouncedSearchTerm);
+  }, [debouncedSearchTerm, onSearch]);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div className="mb-4">
       <input
         type="text"
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={handleSearch}
         placeholder="Rechercher un produit..."
         className={`form-control ${isDarkTheme ? "bg-dark text-light" : ""}`}
       />
